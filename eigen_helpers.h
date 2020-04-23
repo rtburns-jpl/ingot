a0b4c939c91cf8f986fff827a71e45b7bf3cbece
Ryan Burns <rtburns-jpl@users.noreply.github.com>
2020-04-22 18:40:45 -0700

    Working iterator facade!

--- eigen_helpers.h
+++ eigen_helpers.h
@@ -3,19 +3,58 @@
 #define EIGEN_DEFAULT_DENSE_INDEX_TYPE int
 #include <Eigen/Dense>
 
+#include <thrust/iterator/iterator_facade.h>
+
 template<typename T>
 using EMat = typename Eigen::Matrix<T,
       Eigen::Dynamic,
       Eigen::Dynamic,
       Eigen::RowMajor>;
 
-template<typename T>
-class StatevectorColumns {
-    Eigen::Map<EMat<T>> map;
-public:
-    StatevectorColumns(T* ptr, int stride) : map{ptr, 6, stride} {}
+// Convenience typedefs
+template<typename T, int N>
+using MatType = typename Eigen::Matrix<T, N, Eigen::Dynamic, Eigen::RowMajor>;
+template<typename T, int N>
+using ColVal = typename Eigen::Matrix<T, N, 1, Eigen::RowMajor>;
+template<typename T, int N>
+using ColRef = typename Eigen::Block<Eigen::Map<MatType<T, N>, 0>, N, 1>;
 
-    __device__ auto operator()(int i) {
-        return map.col(i);
+template<typename T, int N>
+class ColIter : public thrust::iterator_facade<
+                        ColIter<T, N>,
+                        ColVal<T, N>,
+                        thrust::device_system_tag,
+                        thrust::forward_traversal_tag,
+                        ColRef<T, N>,
+                        Eigen::Index
+                        > {
+public:
+    __host__ __device__
+    ColIter(T* p, int s) : ptr{p}, stride{s} {}
+
+    __host__ __device__
+    auto& advance(int i) {
+        ptr += i;
+        return *this;
     }
+
+    __host__ __device__
+    auto& increment() { return advance(1); }
+
+    // Can only dereference the value in cuda code!
+    __device__
+    auto dereference() const {
+        Eigen::Map<MatType<T, N>> map{ptr, N, stride};
+        return map.col(0);
+    }
+
+    __host__ __device__
+    bool equal(const ColIter& rhs) const {
+        return ptr == rhs.ptr and stride == rhs.stride;
+    }
+
+private:
+    friend class thrust::iterator_core_access;
+    T* ptr;
+    int stride;
 };
--- main.cu
+++ main.cu
@@ -1,5 +1,4 @@
 #include <thrust/device_vector.h>
-#include <thrust/iterator/transform_iterator.h>
 
 #include "eigen_helpers.h"
 
@@ -8,35 +7,48 @@ auto zip_tuple_iters(Ts... ts) {
     return thrust::make_zip_iterator(thrust::make_tuple(ts...));
 }
 
-struct test_operator {
+struct test_transform {
     template<typename T>
     __device__ void operator()(T arg) {
         printf("%g %g %g %g %g %g\n",
                arg[0], arg[1], arg[2],
                arg[3], arg[4], arg[5]);
-
         arg[0] = 0;
-
         printf("%g %g %g %g %g %g\n",
                arg[0], arg[1], arg[2],
                arg[3], arg[4], arg[5]);
     }
 };
+struct test_transform_tuple {
+    template<typename T>
+    __device__ void operator()(T arg) {
+        test_transform{}(thrust::get<1>(arg));
+        auto& t = thrust::get<0>(arg);
+        printf("t = %g\n", t);
+        t = 10;
+        printf("t = %g\n", t);
+    }
+};
 
 int main() {
+    using T = double;
 
     // Allocate initial buffer
-    const int nparticles = 10;
-    thrust::device_vector<double> x{6 * nparticles};
+    const int nparticles = 4;
+    thrust::device_vector<T> x{6 * nparticles};
 
     // Fill with 0...n
     thrust::sequence(x.begin(), x.end());
 
-    thrust::counting_iterator<int> index{0};
-    thrust::transform_iterator<StatevectorColumns<double>, decltype(index)> iter{
-        index,
-        {x.data().get(), nparticles}
-    };
+    ColIter<T, 6> ci{x.data().get(), nparticles};
 
-    thrust::for_each(iter, iter + nparticles, test_operator{});
+    thrust::device_vector<T> t{nparticles};
+    thrust::fill(t.begin(), t.end(), 0);
+    thrust::device_vector<T> h{nparticles};
+    thrust::fill(h.begin(), h.end(), std::numeric_limits<T>::epsilon());
+
+    auto zp = zip_tuple_iters(t.begin(), ci);
+
+    thrust::for_each(zp, zp + nparticles, test_transform_tuple{});
+    thrust::for_each(zp, zp + nparticles, test_transform_tuple{});
 }
