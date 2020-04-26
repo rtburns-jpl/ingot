5ac7aaf57afbf7bbddea33eebb07b8b15e7ad96f
Ryan Burns <rtburns-jpl@users.noreply.github.com>
2020-04-26 16:33:49 -0700

    WIP generalized tableau computation

--- ingot/StackArray.h
+++ ingot/StackArray.h
@@ -11,6 +11,13 @@ public:
     StackArray(StackArray const& other) = default;
 
     CUDA_HOSTDEV
+    static auto Zero() {
+        auto ret = StackArray{};
+        ret.zero();
+        return ret;
+    }
+
+    CUDA_HOSTDEV
     StackArray(T (&x)[N]) {
         for (int i = 0; i < N; i++)
             data[i] = x[i];
@@ -21,6 +28,12 @@ public:
         for (int i = 0; i < N; i++)
             data[i] = e[i];
     }
+
+    CUDA_HOSTDEV void fill(T x) {
+        for (int i = 0; i < N; i++)
+            data[i] = x;
+    }
+    CUDA_HOSTDEV void zero() { fill(0); }
 
     CUDA_HOSTDEV
     operator Eigen::Array<T, N, 1>() const {
--- ingot/method/Tsit5.h
+++ ingot/method/Tsit5.h
@@ -1,67 +1,94 @@
+template<typename T, typename U, int N, typename Func>
+CUDA_HOSTDEV
+auto rk_tableau(T (&a)[N-1][N-1], T (&b)[N], T (&c)[N], Func f,
+                const double t, const double h, U const& y) {
+    auto get_t = [=](int i) {
+        return t + c[i] * h;
+    };
+
+    U k[N];
+
+    auto get_ak = [&](const int i) {
+        U ret = U::Zero();
+        for (int j = 0; j < i; ++j) {
+            ret += a[i-1][j] * k[j];
+        }
+        return ret * h;
+    };
+
+    for (int i = 0; i < N; ++i) {
+        k[i] = f(get_t(i), y + get_ak(i));
+    }
+
+    U bk = U::Zero();
+    for (int i = 0; i < N; ++i) {
+        bk += b[i] * k[i];
+    }
+    return y + bk * h;
+}
+
 struct Tsit5 {
     template<typename Func, typename T, int N>
     CUDA_HOSTDEV auto operator()(Func const& f, const double t, const double h,
                                  StackArray<T, N> const& y) const {
 
-        using Arr = StackArray<T, N>;
         using Coeff = T; // TODO use scalar type for complex
 
         // https://en.wikipedia.org/wiki/Dormand–Prince_method
 
-        static constexpr Coeff c2 = 0.161;
-        static constexpr Coeff c3 = 0.327;
-        static constexpr Coeff c4 = 0.9;
-        static constexpr Coeff c5 = 0.9800255409045097;
-        static constexpr Coeff c6 = 1;
-        static constexpr Coeff c7 = 1;
+        static constexpr int RKN = 7;
 
-        static constexpr Coeff a21 = 0.161;
+        static constexpr Coeff a[RKN-1][RKN-1] {
+            {
+                0.161,
+            }, {
+                -0.008480655492357,
+                0.3354806554923570,
+            }, {
+                2.897153057105494,
+                -6.359448489975075,
+                4.362295432869581,
+            }, {
+                5.32586482843925895,
+                -11.74888356406283,
+                7.495539342889836,
+                -0.09249506636175525,
+            }, {
+                5.86145544294642038,
+                -12.92096931784711,
+                8.159367898576159,
+                -0.071584973281401006,
+                -0.02826905039406838,
+            }, {
+                0.09646076681806523,
+                0.01,
+                0.4798896504144996,
+                1.379008574103742,
+                -3.290069515436081,
+                2.324710524099774,
+            }
+        };
 
-        static constexpr Coeff a31 = -0.008480655492357;
-        static constexpr Coeff a32 = 0.3354806554923570;
+        static constexpr Coeff b[RKN] {
+            0.09646076681806523,
+            0.01,
+            0.4798896504144996,
+            1.379008574103742,
+            -3.290069515436081,
+            2.324710524099774,
+            0,
+        };
 
-        static constexpr Coeff a41 = 2.897153057105494;
-        static constexpr Coeff a42 = -6.359448489975075;
-        static constexpr Coeff a43 = 4.362295432869581;
+        static constexpr Coeff c[RKN] {
+            0,
+            0.161,
+            0.327,
+            0.9,
+            0.9800255409045097,
+            1,
+            1,
+        };
 
-        static constexpr Coeff a51 = 5.32586482843925895;
-        static constexpr Coeff a52 = -11.74888356406283;
-        static constexpr Coeff a53 = 7.495539342889836;
-        static constexpr Coeff a54 = -0.09249506636175525;
-
-        static constexpr Coeff a61 = 5.86145544294642038;
-        static constexpr Coeff a62 = -12.92096931784711;
-        static constexpr Coeff a63 = 8.159367898576159;
-        static constexpr Coeff a64 = -0.071584973281401006;
-        static constexpr Coeff a65 = -0.02826905039406838;
-
-        static constexpr Coeff a71 = 0.09646076681806523;
-        static constexpr Coeff a72 = 0.01;
-        static constexpr Coeff a73 = 0.4798896504144996;
-        static constexpr Coeff a74 = 1.379008574103742;
-        static constexpr Coeff a75 = -3.290069515436081;
-        static constexpr Coeff a76 = 2.324710524099774;
-
-        static constexpr Coeff b1 = 0.09646076681806523;
-        static constexpr Coeff b2 = 0.01;
-        static constexpr Coeff b3 = 0.4798896504144996;
-        static constexpr Coeff b4 = 1.379008574103742;
-        static constexpr Coeff b5 = -3.290069515436081;
-        static constexpr Coeff b6 = 2.324710524099774;
-        static constexpr Coeff b7 = 0;
-
-        const Arr k1 = f(t, y);
-        const Arr k2 = f(t + c2 * h, y + a21 * k1 * h);
-        const Arr k3 = f(t + c3 * h, y + (a31 * k1 + a32 * k2) * h);
-        const Arr k4 = f(t + c4 * h, y + (a41 * k1 + a42 * k2 + a43 * k3) * h);
-        const Arr k5 = f(t + c5 * h, y + (a51 * k1 + a52 * k2 + a53 * k3 +
-                                          a54 * k4) * h);
-        const Arr k6 = f(t + c6 * h, y + (a61 * k1 + a62 * k2 + a63 * k3 +
-                                          a64 * k4 + a65 * k5) * h);
-        const Arr k7 = f(t + c7 * h, y + (a71 * k1 + a72 * k2 + a73 * k3 +
-                                          a74 * k4 + a75 * k5 + a76 * k6) * h);
-
-        return y + (b1 * k1 + b2 * k2 + b3 * k3 + b4 * k4 +
-                    b5 * k5 + b6 * k6 + b7 * k7) * h;
+        return rk_tableau(a, b, c, f, t, h, y);
     }
 };
